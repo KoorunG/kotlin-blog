5cd31bc344cccb54f542372e685c247308f35bd3
KoorunGG <onlyzion10@naver.com>
2024-03-31 02:20:50 +0900

    feat: MethodArgumentNotValidException만 받도록 @ExceptionHandler 변경

--- src/main/kotlin/org/koorung/kotlinblog/controller/ExceptionController.kt
+++ src/main/kotlin/org/koorung/kotlinblog/controller/ExceptionController.kt
@@ -2,6 +2,7 @@ package org.koorung.kotlinblog.controller
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koorung.kotlinblog.exception.exceptionResults
+import org.koorung.kotlinblog.utils.logger
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class ExceptionController {
 
-    val logger = KotlinLogging.logger { }
+    val logger = logger()
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
     fun fieldErrorHandler(e: MethodArgumentNotValidException) =
         if(e.hasFieldErrors()) e.fieldErrors.exceptionResults(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다!") else null
 }
+
